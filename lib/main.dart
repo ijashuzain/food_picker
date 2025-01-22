2de138dbd4bbd980817e58db62c642f9ad8122a0
Ijas Hussain <ijashussain3@gmail.com>
2025-01-22 12:04:23 +0530

    Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:food_picker/src/core/app.dart';
 import 'package:food_picker/src/core/di/di.dart';
 import 'package:food_picker/src/core/services/local_storage_services/local_storage_services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   configureDependencies(Env.dev);
   await LocalStorage.init();
 
